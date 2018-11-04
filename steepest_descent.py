16145b5bc1817ae3d12e213e97f88e90db29e032
Mohammad <hamp.cpas@gmail.com>
2018-11-04 21:34:05 +0330

    fix issue with numpy import

--- steepest_descent.py
+++ steepest_descent.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sympy import Array
 from sympy import derive_by_array
 
@@ -32,7 +33,7 @@ def f1(x1, x2):
     return x1 ** 3 + 25 * x2 ** 2
 
 
-def steepest_descent(f, alpha, max_iter, x0, *args):
+def steepest_descent(f, alpha, max_iter, threshold, X0, *func_vars):
     """The steepest descent algorithm for numerically finding the minimum value of a function,
     based on the gradient of that function. It uses the gradient function (or the scalar derivative,
     if the function is single-valued) to determine the direction in which a function is decreasing most rapidly.
@@ -43,14 +44,16 @@ def steepest_descent(f, alpha, max_iter, x0, *args):
         f (function): function to minimize.
         alpha (float): learning rate.
         max_iter (int): the maximum number of iterations.
-        x0 (array_like): an array like of initial point. For example if the function f is
+        threshold (float): whenever the computed "NEW POINT" [x1, x2, x3, ...] changes is
+            less than threshold (sum of changes of all components), the function stops.
+        X0 (array_like): an array like of initial point. For example if the function f is
             a two variable function, then initial point could be a tuple or list or array
-            with length 2. e.g. x0 = [0, 0]
-        *args (sympy.Symbol): a list of function variables. the derivative of the function
+            with length 2. e.g. X0 = [0, 0]
+        *func_vars (sympy.Symbol): a list of function variables. the derivative of the function
             f will be computed with respect to these parameters.
 
     Returns:
-        list: The history of x0, x1, x2, ..., xn. The first item is the initial point (x0)
+        list: The history of X0, X1, X2, ..., xn. The first item is the initial point (X0)
             and the last one is the best point that minimizes the function f.
 
     Examples:
@@ -58,9 +61,10 @@ def steepest_descent(f, alpha, max_iter, x0, *args):
         >>> def f1(x1, x2): return x1**2 + 25 * x2**2
         >>> x1, x2 = symbols('x1 x2')
         >>> alpha = 0.01
-        >>> x0 = [0.5, 0.5]
+        >>> threshold = 0.001
+        >>> X0 = [0.5, 0.5]
         >>> max_iter = 300
-        >>> X = steepest_descent(f1, alpha, max_iter, x0, x1, x2)
+        >>> X = steepest_descent(f1, alpha, max_iter, threshold, X0, x1, x2)
         >>> print('First 5 points:\n', np.array(X[:5], dtype=np.float))
         First 5 points:
          [[0.5        0.5       ]
@@ -68,20 +72,21 @@ def steepest_descent(f, alpha, max_iter, x0, *args):
          [0.4802     0.125     ]
          [0.470596   0.0625    ]
          [0.46118408 0.03125   ]]
-        >>> print('Minimum point:\n', np.array(X[-1], dtype=np.float))
+        >>> print('Minimum point:\n', np.round(np.array(X[-1], dtype=np.float)))
         Minimum point:
-         [2.77580750e-006 2.40991987e-181]
+         [0. 0.]
     """
-    grad = derive_by_array(f(*args), args)
-    x0 = Array(x0)
-    X = [x0]
-    old_x = x0
-    new_x = old_x - alpha * grad.xreplace(dict(zip(args, old_x)))
+    grad = derive_by_array(f(*func_vars), func_vars)
+    X0 = Array(X0)  # X0 should be an array to support subtraction (old_X - alpha * grad(f, old_X))
+    X = [X0]  # list of history
+    old_X = X0
+    new_X = old_X - alpha * grad.xreplace(dict(zip(func_vars, old_X)))
+
     i = 1
-    while new_x != old_x and i < max_iter:
-        X.append(new_x)
-        old_x = new_x
-        new_x = old_x - alpha * grad.xreplace(dict(zip(args, old_x)))
+    while np.sum(np.abs(new_X - old_X) > threshold) and i < max_iter:
+        X.append(new_X)
+        old_X = new_X
+        new_X = old_X - alpha * grad.xreplace(dict(zip(func_vars, old_X)))
         i += 1
 
     return X
